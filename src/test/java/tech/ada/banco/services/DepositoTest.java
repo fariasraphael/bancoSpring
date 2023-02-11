5d23b3509223517c979a13677773e2f07c4beb84
fariasraphael <farias.raphael@gmail.com>
2023-02-11 15:42:49 -0300

    identação

--- src/test/java/tech/ada/banco/services/DepositoTest.java
+++ src/test/java/tech/ada/banco/services/DepositoTest.java
@@ -18,18 +18,18 @@ class DepositoTest {
     private final ContaRepository repository = Mockito.mock(ContaRepository.class);
     private final Deposito deposito = new Deposito(repository);
 
-    private Conta criaConta (double valor, int numeroDaConta){
+    private Conta criaConta(double valor, int numeroDaConta) {
         Conta conta = new Conta(ModalidadeConta.CC, null);
         conta.deposito(BigDecimal.valueOf(valor));
         when(repository.findContaByNumeroConta(numeroDaConta)).thenReturn(Optional.of(conta));
         assertEquals(BigDecimal.valueOf(valor).setScale(2), conta.getSaldo(),
                 "O valor inicial da conta é " + valor);
-
         return conta;
     }
+
     @Test
     void testeDepositoEmConta() {
-        Conta conta = criaConta(1,10);
+        Conta conta = criaConta(1, 10);
 
         BigDecimal retorno = deposito.executar(10, BigDecimal.TEN);
 
@@ -41,7 +41,7 @@ class DepositoTest {
 
     @Test
     void testeDepositoEmContaNaoEncontrada() {
-        Conta conta = criaConta(1,10);
+        Conta conta = criaConta(1, 10);
 
         try {
             deposito.executar(13, BigDecimal.TEN);
@@ -57,7 +57,7 @@ class DepositoTest {
 
     @Test
     void testeDepositoProblemaDeBancoDeDados() {
-        Conta conta = criaConta(1,10);
+        Conta conta = criaConta(1, 10);
         try {
             deposito.executar(2, BigDecimal.TEN);
             fail("A conta deveria não ter sido encontrada. Por problema de conexao de banco de dados");
@@ -68,7 +68,7 @@ class DepositoTest {
 
     @Test
     void testeDepositoDeValorNegativo() {
-        Conta conta = criaConta(1,10);
+        Conta conta = criaConta(1, 10);
         assertThrows(ResourceNotFoundException.class,
                 () -> deposito.executar(15, BigDecimal.valueOf(-10)));
         verify(repository, times(0)).save(any());
@@ -77,7 +77,7 @@ class DepositoTest {
 
     @Test
     void testeDepositoArredontamentoPraCima() {
-        Conta conta = criaConta(10,10);
+        Conta conta = criaConta(10, 10);
 
         BigDecimal retorno = deposito.executar(10, BigDecimal.valueOf(1.42857));
         verify(repository, times(1)).save(conta);
@@ -85,17 +85,16 @@ class DepositoTest {
                 "O valor de retorno da função deve ser 11.43, a conta inicia com 10 e recebe 1.43");
         assertEquals(BigDecimal.valueOf(11.43).setScale(2), conta.getSaldo());
     }
+
     @Test
     void testeDepositoArredontamentoPraBaixo() {
-        Conta conta = criaConta(10,10);
+        Conta conta = criaConta(10, 10);
 
         BigDecimal retorno = deposito.executar(10, BigDecimal.valueOf(1.42321));
         verify(repository, times(1)).save(conta);
         assertEquals(BigDecimal.valueOf(11.42).setScale(2), retorno,
                 "O valor de retorno da função deve ser 11.42, a conta inicia com 10 e recebe 1.42");
         assertEquals(BigDecimal.valueOf(11.42).setScale(2), conta.getSaldo());
-
-
     }
 
 }
