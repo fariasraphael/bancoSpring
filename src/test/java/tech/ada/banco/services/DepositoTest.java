b1c7a56fb632f149df3d2fd750c17c619c376e7d
fariasraphael <farias.raphael@gmail.com>
2023-02-11 15:41:48 -0300

    Refatoro classe DepositoTeste e crio de testArredontamentoPraCima() e testArredontamentoPraBaixo()

--- src/test/java/tech/ada/banco/services/DepositoTest.java
+++ src/test/java/tech/ada/banco/services/DepositoTest.java
@@ -18,26 +18,30 @@ class DepositoTest {
     private final ContaRepository repository = Mockito.mock(ContaRepository.class);
     private final Deposito deposito = new Deposito(repository);
 
+    private Conta criaConta (double valor, int numeroDaConta){
+        Conta conta = new Conta(ModalidadeConta.CC, null);
+        conta.deposito(BigDecimal.valueOf(valor));
+        when(repository.findContaByNumeroConta(numeroDaConta)).thenReturn(Optional.of(conta));
+        assertEquals(BigDecimal.valueOf(valor).setScale(2), conta.getSaldo(),
+                "O valor inicial da conta é " + valor);
+
+        return conta;
+    }
     @Test
     void testeDepositoEmConta() {
-        Conta conta = new Conta(ModalidadeConta.CC, null);
-        conta.deposito(BigDecimal.ONE);
-        when(repository.findContaByNumeroConta(11)).thenReturn(Optional.of(conta));
-        assertEquals(BigDecimal.valueOf(1), conta.getSaldo(), "O valor inicial da conta é 1");
+        Conta conta = criaConta(1,10);
 
-        BigDecimal resp = deposito.executar(11, BigDecimal.TEN);
+        BigDecimal retorno = deposito.executar(10, BigDecimal.TEN);
 
         verify(repository, times(1)).save(conta);
-        assertEquals(BigDecimal.valueOf(11), resp, "O valor de retorno da função deve ser 11, a conta inicia com 1 e recebe 10");
-        assertEquals(BigDecimal.valueOf(11), conta.getSaldo());
+        assertEquals(BigDecimal.valueOf(11).setScale(2), retorno,
+                "O valor de retorno da função deve ser 11, a conta inicia com 1 e recebe 10");
+        assertEquals(BigDecimal.valueOf(11).setScale(2), conta.getSaldo());
     }
 
     @Test
     void testeDepositoEmContaNaoEncontrada() {
-        Conta conta = new Conta(ModalidadeConta.CC, null);
-        conta.deposito(BigDecimal.ONE);
-        when(repository.findContaByNumeroConta(12)).thenReturn(Optional.of(conta));
-        assertEquals(BigDecimal.valueOf(1), conta.getSaldo(), "O saldo inicial da conta deve ser alterado para 1");
+        Conta conta = criaConta(1,10);
 
         try {
             deposito.executar(13, BigDecimal.TEN);
@@ -48,16 +52,12 @@ class DepositoTest {
 
         verify(repository, times(0)).save(any());
         verify(repository, times(1)).findContaByNumeroConta(anyInt());
-        assertEquals(BigDecimal.valueOf(0), conta.getSaldo(), "O saldo da conta não pode ter sido alterado.");
+        assertEquals(BigDecimal.valueOf(1).setScale(2), conta.getSaldo(), "O saldo da conta não pode ter sido alterado.");
     }
 
     @Test
     void testeDepositoProblemaDeBancoDeDados() {
-        Conta conta = new Conta(ModalidadeConta.CC, null);
-        conta.deposito(BigDecimal.ONE);
-        when(repository.findContaByNumeroConta(14)).thenThrow(RuntimeException.class);
-        assertEquals(BigDecimal.valueOf(1), conta.getSaldo(), "O saldo inicial da conta deve ser alterado para 1");
-
+        Conta conta = criaConta(1,10);
         try {
             deposito.executar(2, BigDecimal.TEN);
             fail("A conta deveria não ter sido encontrada. Por problema de conexao de banco de dados");
@@ -68,14 +68,34 @@ class DepositoTest {
 
     @Test
     void testeDepositoDeValorNegativo() {
-        Conta conta = new Conta(ModalidadeConta.CC, null);
-        conta.deposito(BigDecimal.ONE);
-        when(repository.findContaByNumeroConta(15)).thenReturn(Optional.of(conta));
-        assertEquals(BigDecimal.valueOf(1), conta.getSaldo(), "O saldo inicial da conta deve ser alterado para 1");
-
-        assertThrows(ValorInvalidoException.class, () -> deposito.executar(15, BigDecimal.valueOf(-10)));
+        Conta conta = criaConta(1,10);
+        assertThrows(ResourceNotFoundException.class,
+                () -> deposito.executar(15, BigDecimal.valueOf(-10)));
         verify(repository, times(0)).save(any());
-        assertEquals(BigDecimal.valueOf(1), conta.getSaldo(), "O saldo da conta não se alterou");
+        assertEquals(BigDecimal.valueOf(1).setScale(2), conta.getSaldo(), "O saldo da conta não se alterou");
+    }
+
+    @Test
+    void testeDepositoArredontamentoPraCima() {
+        Conta conta = criaConta(10,10);
+
+        BigDecimal retorno = deposito.executar(10, BigDecimal.valueOf(1.42857));
+        verify(repository, times(1)).save(conta);
+        assertEquals(BigDecimal.valueOf(11.43).setScale(2), retorno,
+                "O valor de retorno da função deve ser 11.43, a conta inicia com 10 e recebe 1.43");
+        assertEquals(BigDecimal.valueOf(11.43).setScale(2), conta.getSaldo());
+    }
+    @Test
+    void testeDepositoArredontamentoPraBaixo() {
+        Conta conta = criaConta(10,10);
+
+        BigDecimal retorno = deposito.executar(10, BigDecimal.valueOf(1.42321));
+        verify(repository, times(1)).save(conta);
+        assertEquals(BigDecimal.valueOf(11.42).setScale(2), retorno,
+                "O valor de retorno da função deve ser 11.42, a conta inicia com 10 e recebe 1.42");
+        assertEquals(BigDecimal.valueOf(11.42).setScale(2), conta.getSaldo());
+
+
     }
 
 }
